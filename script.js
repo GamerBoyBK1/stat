c906dd14eda5687e6b9f962fffd6905386d43156
GamerBoyBK1 <161742908+GamerBoyBK1@users.noreply.github.com>
2025-10-25 11:09:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function checkPanel(elementId) {
     try {
-        await fetch("https://panel1.coramtix.in/", {
+        await fetch("https://panel.coramtix.in/", {
             method: "HEAD",
             mode: "no-cors",
             cache: "no-cache"
@@ -21,7 +21,7 @@ function checkPort(ip, port, elementId) {
 function updateStatus() {
     checkPanel("panel-status");
 
-    checkPort("YOUR_NODE1_IP", 8080, "node1-status");
+    checkPort("38.224.226.9", 8080, "node1-status");
     checkPort("YOUR_NODE2_IP", 8080, "node2-status");
     checkPort("node-in-3.coramtix.in", 8080, "node3-status");
     checkPort("YOUR_NODEBOT_IP", 8080, "node4-status");
